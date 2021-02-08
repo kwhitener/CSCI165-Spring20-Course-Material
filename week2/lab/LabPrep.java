952acf35ecf2e874be703e6f8ff6c3d7046fe405
Ken Whitener <HKW@tc3.edu>
2021-02-08 07:12:08 -0500

    added week2 folder

---
+++ week2/lab/LabPrep.java
@@ -0,0 +1,31 @@
+public class LabPrep{
+
+    public static void main(String[] args) {
+        
+        /* 
+            process command line argument and convert to int
+            let's assume the arg IS a number
+            don't worry about testing the arg to see if it is numeric
+        */
+
+        // Use the static parseInt method from the Integer class
+        int arg = Integer.parseInt(args[0]);
+        // printf requires an explicit line break: \n
+        System.out.printf("\nFirst argument: %d\n", arg);
+
+        /*
+            second arg is a phone number. Let's break it down
+            using String methods. Research the API
+        */
+        
+        String  phone   = args[1];                  // create a String object from arg
+        int     dash    = phone.indexOf("-");       // find the dash
+        String  prefix  = phone.substring(0, dash); // slice off the prefix
+        String  line    = phone.substring(dash + 1, phone.length()); // slice off line number 
+
+        // print the results
+        System.out.println("\nSecond argument: " + phone);  // use println and concatenation. line break automatic
+        System.out.printf("Prefix: %s\n", prefix);          // use printf and explicit line break
+        System.out.printf("Line Number: %s\n", line);       // use printf and explicit line break
+    }
+}
---
+++ week2/lab/UTC.java
@@ -0,0 +1,21 @@
+// Import the correct Calendar class here
+
+public class UTC{
+
+    // constants for raw time conversion
+    // these must be static because they will be called from a "static context" => main
+    static final int MILLISECONDS  = 1000;
+    static final int SECONDS       = MILLISECONDS  / 1000;
+    static final int SEC_PER_MIN   = SECONDS       * 60;
+    static final int MIN_PER_HOUR  = SEC_PER_MIN   * 60;
+    // continue defining symbolic constants for the problem
+
+    public static void main(String[] args) {
+        
+        // write Calendar code here. Research API for details
+
+        // get the command line argument, convert to an integer
+
+        // perform the math.
+    }
+}
